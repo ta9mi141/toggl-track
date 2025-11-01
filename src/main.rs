4938fb67aec41017181372076bb83f136fd48f76
Takumi Ishii <ta9mi141@gmail.com>
2025-11-01 21:38:24 +0900

    Add two options, `--help` and `--version`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, arg_required_else_help = true)]
+struct Cli {}
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
